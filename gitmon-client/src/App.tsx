69cea7b54b1b7c265bf1eed7e955216023aa45f4
mucho7 <ydjm1994@gmail.com>
2025-01-09 09:05:03 +0900

    chore: tailwind 적용 및 랜딩 페이지 CSS 정상화

--- gitmon-client/src/App.tsx
+++ gitmon-client/src/App.tsx
@@ -1,11 +1,12 @@
 import gitmonLogo from "./assets/gitmon.svg";
+import "./index.css";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <div>
-        <a href="https://github.com/ixfp/gitmon" target="_blank">
+    <div className="max-w-screen-xl mx-auto p-8 text-center">
+      <div >
+        <a href="https://github.com/ixfp/gitmon" target="_blank" className="mx-auto">
           <img src={gitmonLogo} className="logo" alt="Gitmon logo" />
         </a>
       </div>
@@ -14,7 +15,7 @@ function App() {
         A blog integrated with GitHub for easy content synchronization and
         updates.
       </p>
-    </>
+    </div>
   );
 }
 
