290b6b30a846e9388f62c6ac66b7131927fe0ea4
mucho7 <ydjm1994@gmail.com>
2025-01-09 10:10:56 +0900

    chore: tanstack 쿼리 설치 및 eslint 추천 룰 수정

--- gitmon-client/eslint.config.js
+++ gitmon-client/eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -7,7 +8,7 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, react.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -23,6 +24,10 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'indent': ['error', 2], 
+      'linebreak-style': ['error', 'unix'], 
+      'semi': ['error', 'always'], 
+      'quotes': ['error', 'single'], 
     },
   },
 )
