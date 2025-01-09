4295718c5a44f402e6024cc28d8c6263feaaf6d1
mucho7 <ydjm1994@gmail.com>
2025-01-09 11:42:11 +0900

    chore: config 수정

--- gitmon-client/vite.config.ts
+++ gitmon-client/vite.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@components': '/src/components',
-      '@layouts': '/src/layouts',
-      '@pages': '/src/pages',
-      '@styles': '/src/styles',
-      '@assets': '/src/assets',
+      "@components": "/src/components",
+      "@layouts": "/src/layouts",
+      "@pages": "/src/pages",
+      "@styles": "/src/styles",
+      "@assets": "/src/assets",
+      "@types": "/src/types",
+      "@hooks": "/src/hooks",
     },
   },
 });
