c7eaa5cd01201508c213942d7a83088d1093a3e5
mucho7 <ydjm1994@gmail.com>
2025-01-10 15:04:46 +0900

    chore: 랜딩에서 블로그로 이어지는 임시 포탈

--- gitmon-client/src/pages/Landing.tsx
+++ gitmon-client/src/pages/Landing.tsx
@@ -1,7 +1,21 @@
+import React, { useRef } from "react";
 import gitmonLogo from "@assets/gitmon.svg";
 import "@styles/landing.css";
+import { useNavigate } from "react-router-dom";
 
 function Landing() {
+  const navigate = useNavigate();
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter" && inputRef.current) {
+      // Enter 키가 눌렸을 때 /temp로 이동\
+      const inputValue = inputRef.current.value.trim();
+      navigate(`/tempBlog/${inputValue}`);
+    }
+  };
+
   return (
     <div className="text-center">
       <div>
@@ -22,6 +36,9 @@ function Landing() {
         A blog integrated with GitHub for easy content synchronization and
         updates.
       </p>
+      <br />
+      <p>Let's Go to...</p>
+      <input ref={inputRef} onKeyDown={handleKeyDown} />
     </div>
   );
 }
