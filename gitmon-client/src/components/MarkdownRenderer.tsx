8ff47d5356e94e74a0081e6fff239506b354eab0
mucho7 <ydjm1994@gmail.com>
2025-01-28 16:33:04 +0900

    fix: props 타입 미지정으로 인한 에러 수정

--- gitmon-client/src/components/MarkdownRenderer.tsx
+++ gitmon-client/src/components/MarkdownRenderer.tsx
@@ -4,33 +4,33 @@ const MarkdownRenderer = ({ markdown }: { markdown: string }) => {
   return (
     <ReactMarkdown
       components={{
-        h1: ({ node, ...props }) => (
+        h1: ({ children, ...props }) => (
           <h1
             {...props}
             className="flex items-center text-lg font-bold px-4 py-2 rounded-md"
           >
-            {props.children}
+            {children}
           </h1>
         ),
-        h2: ({ node, ...props }) => (
+        h2: ({ children, ...props }) => (
           <h2
             {...props}
             className="flex items-center text-lg font-bold px-4 py-2 rounded-md"
           >
-            {props.children}
+            {children}
           </h2>
         ),
-        h3: ({ node, ...props }) => (
+        h3: ({ children, ...props }) => (
           <h3
             {...props}
             className="flex items-center text-lg font-bold px-4 py-2 rounded-md"
           >
-            {props.children}
+            {children}
           </h3>
         ),
-        p: ({ node, ...props }) => (
+        p: ({ children, ...props }) => (
           <p {...props} className="p-2">
-            {props.children}
+            {children}
           </p>
         ),
       }}
