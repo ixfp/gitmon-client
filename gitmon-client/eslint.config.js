47464a78513284c47d4172f363aafc18b1970505
mucho7 <ydjm1994@gmail.com>
2025-01-28 14:42:01 +0900

    chore: eslint import 플러그인 설정

--- gitmon-client/eslint.config.js
+++ gitmon-client/eslint.config.js
@@ -1,33 +1,52 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import importPlugin from "eslint-plugin-import"; // import 플러그인 추가
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, react.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      react.configs.recommended,
+      ...tseslint.configs.recommended,
+    ],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      import: importPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
-      'indent': ['error', 2], 
-      'linebreak-style': ['error', 'unix'], 
-      'semi': ['error', 'always'], 
-      'quotes': ['error', 'single'], 
+      "import/order": [
+        "error",
+        {
+          groups: [
+            ["builtin", "external"],
+            "internal",
+            ["parent", "sibling"],
+            "index",
+          ],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+      indent: ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      semi: ["error", "always"],
+      quotes: ["error", "single"],
     },
-  },
-)
+  }
+);
