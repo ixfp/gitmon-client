5c2ad191d4bf4beba7cf84b76f02aa136956366c
Taehee_Hwang <tevem1207@gmail.com>
2025-02-25 17:44:21 +0900

    feat: 더미 게시물 데이터 추가 및 데이터 가져오기 함수 추가

--- gitmon-next/src/hooks/temp/useDummyData.ts
+++ gitmon-next/src/hooks/temp/useDummyData.ts
@@ -1,18 +1,33 @@
+import { Post } from "@components/Post/types";
 import { useQuery } from "@tanstack/react-query";
 
-const fetchPosts = async (fetchTarget: string) => {
+export const fetchPosts = async <T = Post>(
+  fetchTarget: string
+): Promise<T[]> => {
   try {
     const response = await fetch(`/data/${fetchTarget}.json`);
-    const data = await response.json();
+    const data: T[] = await response.json();
     return data;
   } catch (error) {
-    console.error("Error fetching intro text:", error);
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
+
+export const fetchPost = async <T = Post>(id: string): Promise<T> => {
+  try {
+    const response = await fetch(`http://localhost:3000/data/dummyPost.json`);
+    const data: T = await response.json();
+    return data;
+  } catch (error) {
+    console.error(`Error fetching post ${id}:`, error);
+    throw error;
   }
 };
 
 export const useDummyData = (fetchTarget: string) => {
   return useQuery({
-    queryKey: ["duumyData", fetchTarget],
+    queryKey: ["dummyData", fetchTarget],
     queryFn: () => fetchPosts(fetchTarget),
   });
 };
