33a1827a51d0b026439b13f3c722a0093f62c2de
Taehee_Hwang <89463824+tevem1207@users.noreply.github.com>
2025-08-01 13:33:02 +0000

    hotfix: 깃헙 유저 ID로 조회하도록 급하게 수정

--- gitmon-client/src/app/[id]/[repo]/page.tsx
+++ gitmon-client/src/app/[id]/[repo]/page.tsx
@@ -1,16 +1,31 @@
 import { PostList } from '@components/Post'
-import { fetchPosts } from '@lib/github'
+import { fetchPost } from '@lib/github'
 import { replaceId } from '@lib/utils'
 
 async function BlogMain({ params }: { params: Promise<{ id: string; repo: string }> }) {
-  const { id, repo } = await params
-  const posts = await fetchPosts({ id: replaceId(id), repo })
+  const { id } = await params
+  const { data: urlPosts } = await fetch(
+    `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/v1/posting/github/${replaceId(id)}`,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  )
+    .then(res => {
+      if (!res.ok) {
+        throw new Error('Failed to fetch posts')
+      }
+      console.log(res)
+      return res.json()
+    })
+    .catch(() => null)
 
-  if (!posts) {
+  if (!urlPosts) {
     return (
       <div className="p-8 text-center text-gray-500">글을 불러오는 중 오류가 발생했습니다.</div>
     )
-  } else if (posts.length === 0) {
+  } else if (urlPosts.length === 0) {
     return (
       <div className="p-8 text-center text-gray-500">
         이 저장소에는 아직 글이 없습니다. 첫 글을 작성해보세요!
@@ -18,7 +33,19 @@ async function BlogMain({ params }: { params: Promise<{ id: string; repo: string
     )
   }
 
-  return <PostList posts={posts} />
+  const posts = urlPosts.map(
+    async (post: {
+      id: number
+      title: string
+      githubDownloadUrl: string
+      createdAt: string
+      updatedAt: string
+    }) => {
+      return await fetchPost(post.githubDownloadUrl)
+    },
+  )
+
+  return <PostList posts={await Promise.all(posts)} />
 }
 
 export default BlogMain
