38d5d4c47b18467f73fa8cdd887cd3130279e056
김민찬 <minchan.kim@ahha.ai>
2025-08-04 08:56:23 +0900

    feat: Sentry wizard로 기본설정

---
+++ gitmon-client/sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://34f2119f880464dda5e1a022bb354b77@o4509762350219264.ingest.us.sentry.io/4509762350415872",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
